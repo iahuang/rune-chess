5344df8cc3310a61c60da6725d448f1fd158e03b
iahuang <iahuang@users.noreply.github.com>
2021-03-05 02:10:54 -0500

    ekko

--- src/engine/constants.ts
+++ src/engine/constants.ts
@@ -1,9 +1,13 @@
 import { ChampionDiana } from "./unit/champion/champions/diana";
+import { ChampionEkko } from "./unit/champion/champions/ekko";
 import { ChampionSenna } from "./unit/champion/champions/senna";
 import { ChampionRegistry } from "./unit/champion/champion_registry";
 
 export default class Globals {
     static readonly boardSize = 8;
     static readonly gameVersion = "0.1 indev";
-    static readonly championRegistry = new ChampionRegistry().register("diana", ChampionDiana).register("senna", ChampionSenna);
+    static readonly championRegistry = new ChampionRegistry()
+        .register("diana", ChampionDiana)
+        .register("senna", ChampionSenna)
+        .register("ekko", ChampionEkko);
 }
---
+++ src/engine/unit/champion/champions/ekko.ts
@@ -0,0 +1,50 @@
+import { AbilityIdentifier, AbilityMetric, AbilityMetricType } from "../ability/base_ability";
+import { LocationTargetedAbility } from "../ability/location_target_ability";
+import { SelfTargetedAbility } from "../ability/self_targeted_ability";
+import Champion from "../champion";
+
+class EkkoQ extends LocationTargetedAbility {
+    name = "Timewinder";
+    description =
+        "Ekko places a temporal grenade two squares from him, immediately dealing [DAMAGE] magic damage, and on the next turn returning to the square from which it was casted, dealing [SECONDARY_DAMAGE] magic damage along the way.";
+    identifier = AbilityIdentifier.Q;
+
+    setMetrics() {
+        this.addMetric(AbilityMetricType.Damage, AbilityMetric.withBaseAmount(60).setAPScaling(0.3));
+        this.addMetric(AbilityMetricType.SecondaryDamage, AbilityMetric.withBaseAmount(40).setAPScaling(0.6));
+    }
+}
+
+class EkkoR extends SelfTargetedAbility {
+    name = "Chronobreak";
+    description =
+        "Ekko travels back in time, returning to where he was four turns ago, *displacing* any units where he lands, dealing [DAMAGE] magic damage and healing for [HEALING] HP";
+    identifier = AbilityIdentifier.R;
+
+    setMetrics() {
+        this.addMetric(AbilityMetricType.Damage, AbilityMetric.withBaseAmount(150).setAPScaling(1.5));
+        this.addMetric(AbilityMetricType.Healing, AbilityMetric.withBaseAmount(100).setAPScaling(0.6));
+    }
+
+    onCast() {
+
+    }
+}
+
+export class ChampionEkko extends Champion {
+    constructor() {
+        super({
+            maxHP: 920,
+            armor: 43,
+            magicResistance: 36,
+            abilityPower: 60,
+            attackDamage: 70,
+            attackRange: 1,
+            ranged: false,
+        });
+        this.name = "Ekko";
+        this.championTitle = "The Boy who Shattered Time";
+        this.abilityQ = new EkkoQ(this);
+        this.abilityR = new EkkoR(this);
+    }
+}
