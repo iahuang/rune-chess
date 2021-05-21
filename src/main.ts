3f3875c92fea837951157b57449b51a14616627f
Ian Huang <iahuang@users.noreply.github.com>
2021-05-21 01:43:47 -0400

    clarify main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+/*
+    RUNECHESS
+
+    League of Legends and all related trademarks are property of Riot Games
+    All source code belonging to this project is licensed under the MIT License
+    
+    All assets and art belonging to this project only are free to use for any purpose.
+*/
+
 console.log(`Loading modules...`);
 
 import { BotConfig, RunechessBot } from "./bot/runechess_discord";
@@ -10,14 +19,20 @@ const CONFIG_PATH = "./bot_config.json";
 async function main() {
     Globals.log.getNamespace("Main").write(`Runechess ${Globals.gameVersion}`);
     Globals.programStartupTime = Date.now();
+
+    // Create config file if non-existent
     if (!fs.existsSync(CONFIG_PATH)) {
         fs.writeFileSync(CONFIG_PATH, JSON.stringify(new BotConfig()));
         Globals.log.getNamespace("Runechess-Discord").write("Config file does not exist, creating...");
         process.exit(0);
     }
+
     let config = JSON.parse(fs.readFileSync(CONFIG_PATH, "utf8"));
+
+    // Configure datadragon (riot's asset API)
     let dataDragon = new DataDragon();
     await dataDragon.useLatestGameVersion();
+
     let bot = new RunechessBot(dataDragon, config);
     bot.run();
 }
